10bd0639889b2294f4f32d1ef7c5cd724cdd0c01
Artur Lane <arturlan.us@gmail.com>
2017-02-23 17:00:03 -0500

    Conditions

---
+++ main.js
@@ -0,0 +1,13 @@
+var cardOne = "king";
+var cardTwo = "king";
+var cardThree = "queen";
+var cardFour = "queen";
+
+
+if (cardOne === cardTwo) {
+	alert("You found a match!");
+} else if (cardThree === cardFour) {
+	alert("You found a match!");
+} else if (cardTwo === cardFour) {
+	alert("Sorry, try again.");
+}
