3c5ede7a2d04f90d70a7464e3de1883be2c90848
Artur Lane <arturlan.us@gmail.com>
2017-02-25 21:17:06 -0500

    S

--- main.js
+++ main.js
@@ -4,10 +4,21 @@ var cardThree = "queen";
 var cardFour = "queen";
 
 
-if (cardOne === cardTwo) {
-	alert("You found a match!");
-} else if (cardThree === cardFour) {
-	alert("You found a match!");
-} else if (cardTwo === cardFour) {
-	alert("Sorry, try again.");
-}
+// if (cardOne === cardTwo) {
+// 	alert("You found a match!");
+// } else if (cardThree === cardFour) {
+// 	alert("You found a match!");
+// } else if (cardTwo === cardFour) {
+// 	alert("Sorry, try again.");
+// }
+var boardGame = document.getElementById('game-board');
+var createrCards = function(x) {
+
+	for (var i = 0; i < x.length; i++) {
+		var newElement = document.createElement('div');
+		newElement[i].className = 'card';
+		boardGame.appendChild(newElement);
+	}
+};
+
+createrCards(4);
